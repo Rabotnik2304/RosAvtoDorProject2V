cede6a33f60aa6d73d0550a0c4317392e8fd2c6f
informant <artemiy.solodkin@mail.ru>
2024-02-11 11:35:13 +0500

    Work with api enabling

--- app/src/main/java/com/example/rosavtodorproject2/App.kt
+++ app/src/main/java/com/example/rosavtodorproject2/App.kt
@@ -1,8 +1,11 @@
 package com.example.rosavtodorproject2
 
+import android.R
 import android.app.Application
 import com.example.rosavtodorproject2.ioc.ApplicationComponent
 import com.example.rosavtodorproject2.ioc.DaggerApplicationComponent
+import java.io.IOException
+import java.util.Properties
 
 
 class App : Application() {
@@ -13,6 +16,7 @@ class App : Application() {
     override fun onCreate() {
         super.onCreate()
         sInstance = this
+        BuildConfig.MY_API_KEY
     }
 
     companion object {
